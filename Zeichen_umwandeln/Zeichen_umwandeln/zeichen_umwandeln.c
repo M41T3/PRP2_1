527e7c2341a902dfad570260cc6a5c68f054dc67
MALTES_LAPTOP\Malte <maltemueller1104@gmail.com>
2018-10-18 10:25:26 +0200

    changed zeichen_umwandeln

--- Zeichen_umwandeln/Zeichen_umwandeln/zeichen_umwandeln.c
+++ Zeichen_umwandeln/Zeichen_umwandeln/zeichen_umwandeln.c
@@ -14,10 +14,16 @@ void eingabe(char* ptr);
 void umwandeln(char* ptr);
 void ausgabe(char* ptr);
 
+char eingabe_v(char ptr);
+char umwandeln_v(char ptr);
+void ausgabe_v(char ptr);
+
 int main(void) {
 	char zeichen = ' ';
 
 	eingabe(&zeichen);	
+	fseek(stdin, 0, SEEK_END);
+	ausgabe_v(umwandeln_v(eingabe_v(zeichen)));
 
 	system("PAUSE");
 
@@ -46,3 +52,26 @@ void ausgabe(char* ptr) {
 		printf("Umwandlung des Zeichens!\n");
 	}
 }
+
+char eingabe_v(char ptr) {
+	printf("Bitte geben Sie einen Buchstaben ein: ");
+	scanf("%c", &ptr);	// Abfrage des Zeichens
+
+	ausgabe_v(ptr);
+
+	return ptr;
+}
+
+char umwandeln_v(char ptr) {
+	if (ptr < 122 && ptr > 96) {
+		printf("Umwandlung des Zeichens!\n");
+		ptr = ptr - 32;
+	}
+
+	return ptr;
+}
+
+void ausgabe_v(char ptr) {
+	printf("Das Zeichen ist: %c\n", ptr);	
+}
+
